926d951fef4e0e4c6354645dc8da9838bb4aaade
JackyTongCST <ja_ton@encs.Concordia.ca>
2014-10-29 14:21:24 -0400

    Create pilotA.py

---
+++ pilotA.py
@@ -0,0 +1,22 @@
+# ----- pilotA.py ------
+
+#!/usr/bin/env python
+
+from listenA import *
+from sendB import *
+
+openA()
+
+sendB("Can you here me?")
+
+listenA()
+
+listenA()
+
+sendB("Okay signing out")
+
+sendB("&DISCONNECT&")
+
+listenA()
+
+listenA()
